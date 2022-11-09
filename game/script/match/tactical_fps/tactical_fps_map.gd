a5e8047b8399a18e4f431e449a8eb73c04d433ac
sisipun <sisipun@gmail.com>
2022-11-09 19:25:52 +0300

    Move player with mouse click

--- game/script/match/tactical_fps/tactical_fps_map.gd
+++ game/script/match/tactical_fps/tactical_fps_map.gd
@@ -14,7 +14,5 @@ func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> voi
 			_current_player = null
 		
 		_current_player = player_scene.instance()
-		print((event as InputEventMouseButton).position)
 		_current_player.position = to_local((event as InputEventMouseButton).position)
 		add_child(_current_player)
-		print('hello')
