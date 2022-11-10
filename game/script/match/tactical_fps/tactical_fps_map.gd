89683d08cc38b33796bfcae92ece2480d2bbf312
sisipun <sisipun@gmail.com>
2022-11-10 16:10:41 +0300

    Player movement

--- game/script/match/tactical_fps/tactical_fps_map.gd
+++ game/script/match/tactical_fps/tactical_fps_map.gd
@@ -7,12 +7,12 @@ export (PackedScene) var player_scene = null
 var _current_player: TacticalFpsPlayer = null
 
 
+func _ready() -> void:
+	_current_player = player_scene.instance()
+	_current_player.position = Vector2(0, 0)
+	add_child(_current_player)
+
+
 func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
-	if event is InputEventMouseButton and event.is_pressed():
-		if _current_player:
-			_current_player.queue_free()
-			_current_player = null
-		
-		_current_player = player_scene.instance()
-		_current_player.position = to_local((event as InputEventMouseButton).position)
-		add_child(_current_player)
+	if event is InputEventMouseButton and event.is_pressed() and _current_player:
+		_current_player.move_to(to_local((event as InputEventMouseButton).position))
--- game/script/match/tactical_fps/tactical_fps_player.gd
+++ game/script/match/tactical_fps/tactical_fps_player.gd
@@ -1,3 +1,19 @@
 class_name TacticalFpsPlayer
 extends KinematicBody2D
 
+
+const TARGET_EPSILON = 5
+
+export (float) var speed: float = 150.0
+
+var target: Vector2 = Vector2.ZERO
+
+func _physics_process(delta: float) -> void:
+	var direction: Vector2 = target - position
+	if direction.length() > TARGET_EPSILON:
+		direction = direction.normalized()
+		move_and_slide(speed * direction)
+
+
+func move_to(position: Vector2) -> void:
+	target = position
